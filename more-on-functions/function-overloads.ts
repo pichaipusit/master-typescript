30e02781deab4841748827e3991c355c3a84e9a4
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-02-27 12:34:20 +0700

    7-sol: Overload

--- more-on-functions/function-overloads.ts
+++ more-on-functions/function-overloads.ts
@@ -4,9 +4,21 @@ interface Coordinate {
   y: number;
 }
 
-function parseCoordinateFromObject(obj: Coordinate): Coordinate {
-  return { ...obj };
-}
-function parseCoordinateFromNumber(x: number, y: number): Coordinate {
-  return { x, y };
+function parseCoordinate(obj: Coordinate): Coordinate;
+function parseCoordinate(x: number, y: number): Coordinate;
+function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
+  let coord: Coordinate = {
+    x: 0,
+    y: 0,
+  };
+
+  if (typeof arg1 === "object") {
+    coord = { ...(arg1 as Coordinate) };
+  } else {
+    coord = {
+      x: arg1 as number,
+      y: arg2 as number,
+    };
+  }
+  return coord;
 }
